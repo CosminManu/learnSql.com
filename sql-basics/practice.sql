5c1fa80bae2246f422270f76fed45e5128ab696e
CosminManu <cosmin.manu2000@gmail.com>
2025-02-22 22:24:48 +0200

    sql basics done ch2

--- sql-basics/practice.sql
+++ sql-basics/practice.sql
@@ -32,3 +32,34 @@ where (production_year < 1999 or production_year > 2005)
 
 select * from car where brand = 'Ford'		-- text always with single quotes ' ' // 'Smith' is not same as 'SMITH'
 
+--22.02
+--Select vin, brand, and model of all cars whose brand begins with an F.
+select vin, brand, model from car
+where brand like 'F%';
+
+select vin from car where model like '%s';
+
+select * from car 
+where brand like 'Volk_wagen';
+
+--NULL values = uknown, missing values
+select * from car
+  where price is not null;
+
+select * from car where price is null
+
+-- NULL is not 0
+-- NULL = NULL false
+
+--Select all columns for cars with a tax amount over $2000. The tax amount for all cars is 20% of their price. Multiply the price by 0.2 to get the tax amount.
+select * from car
+where (price * 0.2) > 2000
+
+select * from car
+where production_year between 1999 and 2005 
+  and brand <> 'Volkswagen' 
+  and (model like 'P%' or model like 'F%') 
+  and price is not null
+
+--price <> null incorrect
+--price is not null correct
